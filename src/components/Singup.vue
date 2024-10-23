7761ffffc1739094619589e69f372635ed0c5ae5
behnud <p.behnud@gmail.com>
2024-10-23 12:03:00 -0700

    commit message -m "the first change"

---
+++ src/components/Singup.vue
@@ -0,0 +1,32 @@
+<template>
+    <form>
+        <h3>Sign Up</h3>
+        <div class="form-group">
+            <label>First Name</label>
+            <input type="text" class="form-control" placeholder="First Name"/>
+        </div>
+        <div class="form-group">
+            <label>Last Name</label>
+            <input type="text" class="form-control" placeholder="Last Name"/>
+        </div>
+        <div class="form-group">
+            <label>Email</label>
+            <input type="email" class="form-control" placeholder="Email"/>
+        </div>
+        <div class="form-group">
+            <label>Password</label>
+            <input type="password" class="form-control" placeholder="Password"/>
+        </div>
+        <div class="form-group">
+            <label>Confirm Password</label>
+            <input type="password" class="form-control" placeholder="Confirm Password"/>
+        </div>
+        <button type="submit">Submit</button>
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'Signup'
+}
+</script>
